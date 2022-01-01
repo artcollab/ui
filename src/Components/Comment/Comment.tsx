2a6663f0bf985826969ef0ff17fdef2fc7c606b8
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-01 21:47:08 +0000

    Done sections of the Comment component
    basic comment box, no overflowing text, scrolling for multiple comments.... will add annotations to this when done

--- src/Components/Comment/Comment.tsx
+++ src/Components/Comment/Comment.tsx
@@ -1,9 +1,35 @@
 import React from "react";
-import { comment } from "../../Types/Comment";
+import {comment} from "../../Types/Comment";
+import {Avatar, Box, Container, Grid, Paper} from "@mui/material";
 
-function Comment(props : comment | undefined) {
+function Comment(props: comment) {
+
+    let userName = props?.user.name
+    let userAvatar = <Avatar sx={{bgcolor: "#000080"}}>{props?.user.name.split(' ')[0][0]}</Avatar>
+    let userText = props?.text
+
     return (
-        <></>
+        <Container maxWidth="sm">
+            <Box sx={{
+                wordBreak: 'break-word',
+                overflow: 'scroll',
+                padding: '10px',
+                height: '300px',
+                width: '250px',
+                border: '3px solid black'
+            }}>
+                <Paper sx={{maxWidth: 225, my: 1, mx: 'auto', p: 2}}>
+                    <Grid container wrap="nowrap" spacing={1.5}>
+                        <Grid item>
+                            {userAvatar}
+                        </Grid>
+                        <Grid item xs>
+                            {userName} {userText}
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Box>
+        </Container>
     )
 }
 

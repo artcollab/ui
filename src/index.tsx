4183f48b5fb3f5046b1766a3e286159dfa1b90eb
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-01 21:43:15 +0000

    Updated index.tsx with Comment component
    placeholder values used currently

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Feed from './Components/Feed/Feed';
 import Header from './Components/Header/Header';
+import Comment from './Components/Comment/Comment';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
     <Feed />
+    <Comment text={ " - This is a comment!" } user={ {name : "Cade", thumbnail : "Thumbnail"} } />
   </React.StrictMode>,
   document.getElementById('root')
 );

a7ed45abcaa93ffb644a9e62d28d4c4628397209
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-05 12:53:19 +0000

    Initial Post code, layout testing

--- src/Components/Post/Post.tsx
+++ src/Components/Post/Post.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import './Post.scss';
 import { post } from "../../Types/Post";
+import {Paper} from "@mui/material";
 
 function Post(props : post | undefined) {
     return (
-        <></>
+        <Paper elevation={3} className="mainContainer">
+
+        </Paper>
     )
 }
 

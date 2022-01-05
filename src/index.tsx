6b600dbbe26fdf37c54477d452e10d48e9515f2a
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-05 12:52:01 +0000

    Post component included.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Feed from './Components/Feed/Feed';
 import Header from './Components/Header/Header';
+import Feed from './Components/Feed/Feed';
 import Comment from './Components/Comment/Comment';
+import Post from './Components/Post/Post';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
     <Feed />
     <Comment text={ "This is a comment!" } user={ {name : "DrawDojo", thumbnail : "Thumbnail", color : "#fef3bd"} } />
+    <Post user={{
+            name: "",
+            thumbnail: "",
+            color: ""
+        }} image={undefined} caption={""} likes={[]} comments={[]} />
+
   </React.StrictMode>,
   document.getElementById('root')
 );

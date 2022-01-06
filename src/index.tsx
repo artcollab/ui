eff51b62440286eea363109d6aded2243145dc08
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-06 16:44:31 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Header from './Components/Header/Header';
-import Comment from './Components/Comment/Comment';
 import Post from './Components/Post/Post';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
-    {/*<Feed />*/}
     <Post/>
 
   </React.StrictMode>,

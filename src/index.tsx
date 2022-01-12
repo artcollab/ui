89ff76d137aef2dd1eac9c242f9eafd2141caacd
JamesBeach2 <james.beach.98@gmail.com>
2022-01-12 20:48:02 +0000

    add initial login components, too similar to mui, no submit logic yet, no form validation

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Header from './Components/Header/Header';
-import Post from './Components/Post/Post';
+import Login from './Components/Login/Login';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
-    <Post/>
+    <Login/>
 
   </React.StrictMode>,
   document.getElementById('root')

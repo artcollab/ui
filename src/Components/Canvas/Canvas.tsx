dee3c2b6d0ce7736294d8d3c861545ae859fcb9d
JamesBeach2 <james.beach.98@gmail.com>
2022-01-18 23:06:59 +0000

    add free drawing functionality, add brush size, improve appearance

--- src/Components/Canvas/Canvas.tsx
+++ src/Components/Canvas/Canvas.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { fabric } from 'fabric';
 import "./Canvas.scss";
-import { Paper, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { Button, ButtonGroup, Divider, Input, Paper, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { ToolBarItem } from '../../Types/ToolbarItems';
 import MouseIcon from '@mui/icons-material/Mouse';
 import BrushIcon from '@mui/icons-material/Brush';
@@ -12,20 +12,33 @@ import CircleIcon from '@mui/icons-material/Circle';
 function Canvas() {
     const [canvas, setCanvas] = useState<fabric.Canvas | undefined>(undefined);
     const [colour, setColour] = useState("#000000");
+    const [brushSize, setBrushSize] = useState(1);
     const [currentTool, setCurrentTool] = useState<ToolBarItem>("move");
     const ToggleTool = (e: React.MouseEvent<HTMLElement>, newTool: string) => {
-        setCurrentTool(newTool as ToolBarItem);
+        if (canvas) {
+            if (currentTool === "paint") canvas.isDrawingMode = false;
+            if (newTool === "paint" && !canvas.isDrawingMode) canvas.isDrawingMode = true;
+            setCurrentTool(newTool as ToolBarItem);
+        }
     }
 
     function initCanvas(): fabric.Canvas {
         return (
-            new fabric.Canvas('canv', {
-                height: 700,
+            new fabric.Canvas("canvas", {
+                height: 400,
                 width: 700,
                 backgroundColor: 'white',
             })
         );
     }
+
+    useEffect(() => {
+        if (canvas) {
+            canvas.freeDrawingBrush.color = colour;
+            canvas.freeDrawingBrush.width = brushSize;
+        }
+
+    }, [colour, brushSize]);
 
     useEffect(() => {
         setCanvas(initCanvas());
@@ -38,15 +51,14 @@ function Canvas() {
         let mouseY = pointer!.y;
 
         let defaultSize = 100;
-        let defaultRadius = 50
 
         if (currentTool === "square") {
             object = new fabric.Rect({
                 height: defaultSize,
                 width: defaultSize,
                 fill: colour,
-                left: mouseX - (defaultSize/2),
-                top: mouseY - (defaultSize/2)
+                left: mouseX - (defaultSize / 2),
+                top: mouseY - (defaultSize / 2)
             });
         }
 
@@ -55,54 +67,68 @@ function Canvas() {
                 width: defaultSize,
                 height: defaultSize,
                 fill: colour,
-                left: mouseX - (defaultSize/2),
-                top: mouseY - (defaultSize/2)
+                left: mouseX - (defaultSize / 2),
+                top: mouseY - (defaultSize / 2)
             })
         }
         else if (currentTool === "ellipse") {
             object = new fabric.Circle({
-                radius: defaultRadius,
+                radius: defaultSize / 2,
                 fill: colour,
-                left: mouseX - (defaultSize/2),
-                top: mouseY - (defaultSize/2)
+                left: mouseX - (defaultSize / 2),
+                top: mouseY - (defaultSize / 2)
             })
         }
 
-        if (currentTool != "move") {
-            canvas!.add(object!)
-            canvas!.renderAll()
+        if (currentTool !== "move") {
+            canvas?.add(object!)
+            canvas?.renderAll()
         }
 
         setCurrentTool("move");
     };
 
     return (
-        <Paper className='squareContainer'>
-            <div>
-                <div>
-                    <input type="color" value={colour} onChange={(e) => setColour(e.target.value)} />
-                    <button type='button' name='clear' onClick={() => canvas?.clear()}>
-                        Clear
-                    </button>
-                    <ToggleButtonGroup
-                        exclusive
-                        value={currentTool}
-                        onChange={ToggleTool}
-                    >
+        <>
+            <Paper className="toolBarContainer">
+                    <ToggleButtonGroup exclusive value={currentTool} onChange={ToggleTool}>
                         <ToggleButton value="move"><MouseIcon /></ToggleButton>
                         <ToggleButton value="paint"><BrushIcon /></ToggleButton>
                         <ToggleButton value="square"><CropSquareIcon /></ToggleButton>
                         <ToggleButton value="triangle"><ChangeHistoryIcon /></ToggleButton>
                         <ToggleButton value="ellipse"><CircleIcon /></ToggleButton>
                     </ToggleButtonGroup>
-                </div>
+                    <Divider flexItem orientation="vertical" />
+                    <ButtonGroup>
+                        <Button type='button' name='clear' onClick={() => canvas?.clear()}>Clear</Button>
+                        <Button><Input type="color" className="colorInput" value={colour} onChange={(e) => setColour(e.target.value)} disableUnderline />Colour</Button>
+                        <Button >
+                            <Input
+                                value={brushSize}
+                                onChange={(e: any) => { setBrushSize(e.target.value) }}
+                                size="small"
+                                disableUnderline
+                                inputProps={{
+                                    step: 1,
+                                    min: 1,
+                                    max: 100,
+                                    type: "number"
+                                }}
+                                sx={{ width: "3rem", marginRight: "0.5rem" }}
+                            />
+                            Brush Size
+                        </Button>
+                    </ButtonGroup>
+            </Paper>
+            <Paper className='squareContainer'>
+                <div>
+                    <div onClick={currentTool !== "paint" ? addObject : () => { }} >
+                        <canvas id="canvas" />
+                    </div>
 
-                <div onClick={addObject} >
-                    <canvas id='canv'/>
                 </div>
-
-            </div>
-        </Paper>
+            </Paper>
+        </>
     )
 }
 

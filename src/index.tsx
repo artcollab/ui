ba101de5a633ee61a912395e1f48dfe11558c527
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-27 13:09:03 +0000

    I broke the tests whoops

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Header from './Components/Header/Header';
 import Canvas from './Components/Canvas/Canvas';
-import Post from './Components/Post/Post';
-import {post} from "./Types/Post";
-
-const tempPost : post = {
-    user: {
-        name: " ",
-        thumbnail: " ",
-        color: " ",
-    },
-    image: " ",
-    caption: " ",
-    likes: [],
-    comments: []
-}
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
     <Canvas room={'exampleRoom'}/>
-    <Post Post={tempPost}/>
 
   </React.StrictMode>,
   document.getElementById('root')

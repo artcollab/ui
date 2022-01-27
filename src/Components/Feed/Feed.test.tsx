7d601eb1600537e9cda3f401cdc534bfd48caa92
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-27 20:05:26 +0000

    Initial feed test
    scroll button test done

--- src/Components/Feed/Feed.test.tsx
+++ src/Components/Feed/Feed.test.tsx
@@ -1,5 +1,42 @@
+import {render, waitFor, screen, fireEvent} from "@testing-library/react";
+import Feed from "./Feed";
+import React from "react";
+import {post} from "../../Types/Post";
+
 export {}
 
-it("placeholder test", () => {
-    expect(true)
-});
+const tempPost : post = {
+    user: {
+        name: " ",
+        thumbnail: " ",
+        color: " ",
+    },
+    image: " ",
+    caption: " ",
+    likes: [],
+    comments: []
+}
+
+/* renders the Feed component for testing */
+const feedComponent = render(<Feed posts={[tempPost]}/>);
+
+/* tests the Feed's scroll button */
+it("scroll button test", () => {
+
+    /* fireEvent makes the window scroll down to the Y position of 350,
+    this the threshold where the scroll button will show  */
+    fireEvent.scroll(window, { target: { scrollY: 350 } })
+
+    /* spys on the window method of 'scroll' */
+    jest.spyOn(window, 'scroll')
+
+    /* gets the scroll button element from the feed component via test id */
+    const scrollButton = feedComponent.getByTestId("scroll-button-test");
+
+    /* fireEvent will simulate a click on the scroll button */
+    fireEvent.click(scrollButton);
+
+    /* once the scroll button is clicked the user should be at the top of the page */
+    expect(window.scrollY === 0).toBeTruthy();
+
+})

2f3d48712b0a7b42a24d5ff5a11e75ad82826d5b
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-29 20:46:07 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Header from './Components/Header/Header';
-import Feed from './Components/Feed/Feed';
-import {post} from "./Types/Post";
 import Register from './Components/Register/Register';
-
-const tempPost : post = {
-    id: 0,
-    user: {
-        name: " ",
-        thumbnail: " ",
-        color: " ",
-    },
-    image: " ",
-    caption: " ",
-    likes: [],
-    comments: [],
-}
-
+        
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
-    <Feed posts={[tempPost]}/>
     <Register />
   </React.StrictMode>,
   document.getElementById('root')

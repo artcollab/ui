26cf7b46af0a6fe1aabfcc9cc35953b7b58b6a9e
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-29 20:57:08 +0000

    FEED SHOW UP AGAIN 😃

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Header from './Components/Header/Header';
 import Register from './Components/Register/Register';
+import Feed from './Components/Feed/Feed';
         
+const tempPost : post = {
+    id: 0,
+    user: {
+        name: " ",
+        thumbnail: " ",
+        color: " ",
+    },
+    image: " ",
+    caption: " ",
+    likes: [],
+    comments: [],
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
-    <Register />
+    <Register/>
+    <Feed posts={[tempPost]}/>
   </React.StrictMode>,
   document.getElementById('root')
 );

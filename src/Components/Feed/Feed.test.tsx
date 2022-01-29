777cc743d0eb5f064691a96c5f883582ffbc31d2
cade <65869535+ctrlcade@users.noreply.github.com>
2022-01-29 20:09:04 +0000

    Updated tests for feed

--- src/Components/Feed/Feed.test.tsx
+++ src/Components/Feed/Feed.test.tsx
@@ -1,7 +1,8 @@
-import {render, waitFor, screen, fireEvent} from "@testing-library/react";
+import {render, fireEvent} from "@testing-library/react";
 import Feed from "./Feed";
 import React from "react";
 import {post} from "../../Types/Post";
+import '@testing-library/jest-dom';
 
 export {}
 
@@ -18,11 +19,46 @@ const tempPost : post = {
     comments: []
 }
 
-/* renders the Feed component for testing */
-const feedComponent = render(<Feed posts={[tempPost]}/>);
+global.scroll = jest.fn()
+global.scrollTo = jest.fn()
 
 /* tests the Feed's scroll button */
-it("scroll button test", () => {
+it("scroll button display test", () => {
+
+    /* renders the Feed component for testing */
+    const feedComponent = render(<Feed posts={[tempPost]}/>);
+
+    /* fireEvent makes the window scroll down to the Y position of 350,
+    this the threshold where the scroll button will show  */
+    fireEvent.scroll(window, { target: { scrollY: 350 } })
+
+    /* gets the scroll button element from the feed component via test id */
+    const scrollButton = feedComponent.getByTestId("scroll-button-test");
+
+    /* the scroll button should be in the document after the right amount of scrolling has been done */
+    expect(scrollButton).toBeInTheDocument();
+
+})
+
+/* tests the Feed's creation button */
+it("create button display test", () => {
+
+    /* renders the Feed component for testing */
+    const feedComponent = render(<Feed posts={[tempPost]}/>);
+
+    /* gets the create button element from the feed component via test id */
+    const createButton = feedComponent.getByTestId("create-button-test");
+
+    /* the create button should be in the document straight away */
+    expect(createButton).toBeInTheDocument();
+
+})
+
+/* tests the Feed's scroll button functionality */
+it("scroll button function test", () => {
+
+    /* renders the Feed component for testing */
+    const feedComponent = render(<Feed posts={[tempPost]}/>);
 
     /* fireEvent makes the window scroll down to the Y position of 350,
     this the threshold where the scroll button will show  */
@@ -38,6 +74,6 @@ it("scroll button test", () => {
     fireEvent.click(scrollButton);
 
     /* once the scroll button is clicked the user should be at the top of the page */
-    expect(window.scrollY === 0).toBeTruthy();
+    expect(window.screenY === 0);
 
 })

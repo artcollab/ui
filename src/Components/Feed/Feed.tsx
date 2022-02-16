233cc522e1bc330ef95f90843608ab8880c57cef
cade <65869535+ctrlcade@users.noreply.github.com>
2022-02-16 21:45:46 +0000

    Create canvas popover implemented

--- src/Components/Feed/Feed.tsx
+++ src/Components/Feed/Feed.tsx
@@ -1,9 +1,13 @@
 import React, {lazy, Suspense, useState} from "react";
 import './Feed.scss'
-import {Button, CircularProgress, IconButton} from '@mui/material';
+import {Button, CircularProgress, IconButton, Switch, Container,
+        Typography, Popover, Radio, RadioGroup, FormControlLabel,
+        FormControl} from '@mui/material';
 import smoothscroll from 'smoothscroll-polyfill';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import GestureOutlinedIcon from '@mui/icons-material/GestureOutlined';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import CreateIcon from '@mui/icons-material/Create';
 import {post} from "../../Types/Post";
 import { useNavigate } from "react-router-dom";
 
@@ -15,7 +19,6 @@ let postsDisplayed = 2;
 
 /* initialises the smoothscroll package, this is required for smooth scrolling on browsers such as Safari */
 smoothscroll.polyfill();
-
 
 const tempPost : post = {
     id: 0,
@@ -34,7 +37,15 @@ const tempPost : post = {
     }
 }
 
+/* alphabet constant which includes alphabetical characters from a-z & A-Z */
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+/* initial empty string for the roomID */
+let roomID = '';
+
 function Feed() {
+
+    /* for react routing to other pages */
     const navigate = useNavigate();
 
     /* React hook for the array of posts, used for generating and keeping track of how many posts are displayed */
@@ -48,7 +59,33 @@ function Feed() {
 
         /* a new post is fetched & displayed onto the feed for the user */
         setDisplayPosts(Array.from(Array(postsDisplayed).keys()));
+    }
 
+    /* React hook for handling the canvas creation popover */
+    const [popover, setPopover] = useState(false);
+
+    /* opens the popover when the canvas creation button is clicked, each time this is called
+    * a new randomly generated roomID will be created and displayed */
+    function handlePopover() {
+
+        /* button has been clicked so popover can be shown */
+        setPopover(true);
+
+        /* sets the roomID to empty string again to avoid a previously generated string appearing again */
+        roomID = ''
+
+        /* generates 6 random alphabetical characters [a-Z] as a string */
+        while (roomID.length < 6) {
+            roomID += alphabet[Math.floor(Math.random() * alphabet.length)]
+        }
+    }
+
+    /* when the user clicks anywhere else on the screen outside of the popover this will be
+    * called and will close the popover */
+    function handlePopoverClose() {
+
+        /* user has clicked somewhere else on-screen so popover can be hidden */
+        setPopover(false);
     }
 
     /* React hook to handle when the up arrow is shown/hidden  */
@@ -66,7 +103,6 @@ function Feed() {
         else {
             setUpArrow(false)
         }
-
     })
 
     /* when called this will scroll the window to the top of the page */
@@ -74,7 +110,6 @@ function Feed() {
 
         /* uses smooth scrolling behaviour instead of instantly transitioning to the top */
         window.scroll({top: 0, behavior: 'smooth'});
-
     }
 
     return (
@@ -85,7 +120,7 @@ function Feed() {
 
                 {/* Suspense tag, on fallback will display a loading circle icon to depict the loading of the feed component*/}
                 <Suspense
-                    fallback={<div className={"loadingIcon"}><CircularProgress size={125} sx={{color: "#ffccac"}}/>
+                    fallback={<div className={"loadingIcon"}><CircularProgress className={"loadingGraphic"} size={125}/>
                     </div>}>
 
                     {/* uses a nested map to fetch and display the posts */}
@@ -103,9 +138,55 @@ function Feed() {
 
             </div>
 
-            {/* IconButton for the create functionality, this should generate a modal which allows for canvas options */}
-            <IconButton onClick={() => navigate("/canvas")} size={"small"} className={'createButton'}
-                        data-testid="create-button-test"><GestureOutlinedIcon/></IconButton>
+            {/* IconButton for the create functionality, on click this will show a popover which will provide canvas options */}
+            <IconButton onClick={handlePopover} size={"small"} className={'createButton'} data-testid="create-button-test">
+                <GestureOutlinedIcon/>
+            </IconButton>
+
+
+            {/* Popover component which allows for canvas customisation */}
+            <Popover open={popover} onClose={handlePopoverClose}
+                     anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+                     style={{marginLeft: 40, textAlign: "center"}}
+            >
+
+                {/* container for the canvas options included in the popover menu */}
+                <Container sx={{p: 1}}>
+
+                    {/* canvas creation title*/}
+                    <Typography className={"popoverTitle"}>Canvas Creation</Typography>
+
+                    {/* roomID provided to user, a random alphabetical string of 6 characters */}
+                    <Typography className={"popoverOptions"}>Room ID: {roomID}</Typography>
+
+                    {/* form for the canvas size options */}
+                    <FormControl className={"popoverOptions"}>
+
+                        {/* section title */}
+                        <Typography>Canvas Size: </Typography>
+
+                        {/* radio group detailing each of the canvas options available to the user (square, portrait, rectangle) */}
+                        <RadioGroup style={{marginTop: 10, marginLeft: 15}} row defaultValue="Square">
+                            <FormControlLabel value="Square" control={<Radio />} label="Square" />
+                            <FormControlLabel value="Portrait" control={<Radio />} label="Portrait" />
+                            <FormControlLabel value="Rectangle" control={<Radio />} label="Rectangle" />
+                        </RadioGroup>
+                    </FormControl>
+
+                    {/* private option switch, only host can invite people */}
+                    <Typography className={"popoverOptions"}>Private: <Switch/></Typography>
+
+                    {/* invitation option, includes clickable button which will be used to invite collaborators */}
+                    <Typography className={"popoverOptions"}>Invite:
+                        <IconButton style={{color: "#42342c"}} size={"small"}><PersonAddIcon/></IconButton>
+                    </Typography>
+
+                    {/* create canvas button which navigates the user to the canvas menu */}
+                    <Button className={"createCanvas"} onClick={() => navigate("/canvas")}>CREATE &nbsp;
+                        <CreateIcon/>
+                    </Button>
+                </Container>
+            </Popover>
 
             {/* when showUpArrow is true then the button can be displayed & its functionality can be utilised */}
             {showUpArrow &&
@@ -113,9 +194,7 @@ function Feed() {
                 <IconButton size={"small"} className={'scrollButton'} data-testid="scroll-button-test"
                             onClick={scrollToTop}><ArrowUpwardIcon/></IconButton>
             </span>)}
-
         </>
-
     )
 
 }

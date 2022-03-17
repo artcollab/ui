f896a699324309813bd14267e17a388838414be1
JamesBeach2 <james.beach.98@gmail.com>
2022-03-17 22:21:11 +0000

    fix conflict

--- src/Components/Comment/Comment.tsx
+++ src/Components/Comment/Comment.tsx
@@ -50,6 +50,7 @@ function Comment({ commentsList, focused = false, setFocused, post_id }: comment
             author: User,
             content: textValue,
             post_id: post_id
+        }
 
         sendHTTPRequest("POST", "/posts/comment", JSON.stringify(userComment), JSON.parse(at!)).then(() => {
             /* creates a comment array containing the comments within the commentList */
